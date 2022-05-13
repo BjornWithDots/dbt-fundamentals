8f8f8a733935f793afdc9c9e2f6b005059f16c6e
Björn Blomberg <bjorn.blomberg@netonnet.com>
2022-05-13 17:06:56 +0200

    small fixes

--- macros/limit_dev_data.sql
+++ macros/limit_dev_data.sql
@@ -1,6 +1,7 @@
 -- Macro to filter out data when in dev environment
+--where {{ filter_column }} >= dateadd('day',- {{ numberofdays }}, current_timestamp) --MSSQL
 {% macro limit_dev_data(filter_column, numberofdays=3) %}
 {%- if target.name == 'dev' -%}
-where {{ filter_column }} >= dateadd('day',- {{ numberofdays }}, current_timestamp)
+where {{ filter_column }} >= DATE_SUB(CURRENT_DATE(), INTERVAL {{ numberofdays }} DAY) --BQ
 {%- endif %}
 {%- endmacro %}
